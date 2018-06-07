60047ba249f42bfc85902832530fe15ad4569775
Leon <wxmaxx@gmail.com>
2018-06-07 05:28:09 -0500

    Update maxwell-assignment-3.4.js

--- maxwell-assignment-3.4.js
+++ maxwell-assignment-3.4.js
@@ -32,43 +32,67 @@
 */ 
 
 // test variables for the for loop
-var testFor1 = 6;
-var testFor2 = 4;
-var testFor3 = 7;
-var testFor4 = 4;
-var testFor5 = 8;
-var testFor6 = 9;
-var testFor7 = 7;
-var testFor8 = 6;
-var testFor9 = 3;
 
-// Test variables for the while loop
-
-
-
-// for loop
-console.log("-- Displaying for loop --");
-
-
-
-
-// while loop
-console.log("\n-- Displaying while loop --"); 
-
-
-
-
-// Reused functions from exercise-3.2.js
-
-// functions
 
 //function compares 2 arguments to see if they match or not; true if match, false if no match
+match.arg1 = 6;
+match.arg2= randomNumber();
+
 function match(arg1, arg2) {
   if (arg1 === arg2)
     return true
   else
     return false
 } 
+
+
+// ** DO NOT MODIFY OR REMOVE THIS FUNCTION **
+function randomNumber() {
+    return Math.floor((Math.random() * 10) + 1); 
+}
+
+// Test variables for the while loop
+
+
+
+// // for loop
+// console.log("-- Displaying for loop --");
+// var x = randomNumber(2);
+
+// for (var x = 0; x < 10; x++) {
+//     console.log(x);
+// }
+
+// function randomNumber() {
+//     return Math.floor((Math.random() * 10) + 1); 
+// }
+
+
+// // while loop
+// console.log("\n-- Displaying while loop --");
+// var index = 0;
+
+// while (index < 10) {
+//     console.log(index);
+//     index++;
+// }
+
+
+
+// Reused functions from exercise-3.2.js
+
+//function compares 2 arguments to see if they match or not; true if match, false if no match
+match.arg1 = 6;
+match.arg2 = 6;
+
+function match(arg1, arg2) {
+  if (arg1 === arg2)
+    return true
+  else
+    return false
+}
+
+ console.log(match());
 
 //function tests if arg1 and arg2 match and logs result
 function logMatch(arg1, arg2) {
@@ -82,32 +106,17 @@ function logMismatch(arg1, arg2) {
 
 
 // Output from the match() function... 
-console.log(match("A", "B"));
-console.log(match(2, 2)); 
+console.log(match("arg1", "arg2"));
+console.log(match(2, 3)); 
 
 
 // Conditional "if...else" statements. Include checks for all six (6) test variables 
-//the first if statement checks the first two variables
-if (match(testVar1, testVar2)) {
-    logMatch(testVar1, testVar2);
-} else {
-    logMismatch(testVar1, testVar2);
+
+// ** DO NOT MODIFY OR REMOVE THIS FUNCTION **
+function randomNumber() {
+    return Math.floor((Math.random() * 10) + 1); 
 }
 
-//the second if statement checks the third and fourth variables
-if (match(testVar3, testVar4)) {
-    logMatch(testVar3, testVar4);
-} else {
-    logMismatch(testVar3, testVar4);
-}
-
-//the third if statement checks the fifth and sixth variables
-if (match(testVar5, testVar6)) {
-    logMatch(testVar5, testVar6);
-} else {
-    logMismatch(testVar5, testVar6);
-}
-
-
+// end program
 
 
