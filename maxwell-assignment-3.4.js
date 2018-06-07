f6fd45056df8bc243bc0dfc80df0c3b0f754d533
Leon <wxmaxx@gmail.com>
2018-06-07 01:53:39 -0500

    Starter code

---
+++ maxwell-assignment-3.4.js
@@ -0,0 +1,53 @@
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Assignment 3.4
+    Today's Date
+
+    -- Displaying for loop --
+    6 does not match 4!
+    6 does not match 7!
+    6 does not match 4!
+    6 does not match 8!
+    6 does not match 9!
+    6 does not match 7!
+    6 does match 6!
+    6 does not match 3!
+
+    -- Displaying while loop --
+    6 does not match 5!
+    6 does not match 10!
+    6 does not match 8!
+    6 does not match 10!
+    6 does not match 10!
+    6 does match 6!
+    6 does not match 4!
+    6 does not match 3!
+    6 does match 6!
+    6 does match 6!
+
+*/ 
+
+// test variable
+
+
+
+
+// for loop
+console.log("-- Displaying for loop --");
+
+
+
+
+// while loop
+console.log("\n-- Displaying while loop --"); 
+
+
+
+
+// Reused functions from exercise-3.2.js
+
+
