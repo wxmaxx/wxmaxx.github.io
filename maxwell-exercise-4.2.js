2820ea6801c8d9713df296c981abf0434b49ab0a
Leon <wxmaxx@gmail.com>
2018-06-13 04:07:06 -0500

    final changes with comments

--- maxwell-exercise-4.2.js
+++ maxwell-exercise-4.2.js
@@ -19,7 +19,7 @@
 // Declare an array of fruits and assign it to the variable fruits
 var fruits = ["Apple", "Orange", "Banana", "Mango", "Pear"];
 
-// function
+// getFruit function calls in the fruits array, loops through the list using a for loop, logs the output
 function getFruit(arr) {
  for (var k = 0; k < arr.length; k++) {
   console.log(arr[k]);
@@ -29,8 +29,6 @@ function getFruit(arr) {
 
 // Output from the getFruit() function
 getFruit(fruits);
-
-
 
 
 // end program
