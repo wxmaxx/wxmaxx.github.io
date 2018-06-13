ea757428d22c99f50e11fa4e86f1c6bbe196fb6e
Leon <wxmaxx@gmail.com>
2018-06-13 03:52:00 -0500

    Starter code for exercise 4.2

---
+++ maxwell-exercise-4.2.js
@@ -0,0 +1,32 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 4.2
+ Today's Date
+
+ Apple
+ Orange
+ Banana
+ Mango
+ Pear
+ */
+
+// start program
+
+
+
+// function
+function getFruit() {
+}
+
+
+// Output from the getFruit() function
+
+
+
+
+
+// end program
