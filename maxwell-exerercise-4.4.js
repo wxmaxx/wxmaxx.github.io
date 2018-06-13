9657677ae1400746f9bd7342fdab0dd18221f04d
Leon <wxmaxx@gmail.com>
2018-06-13 04:59:28 -0500

    boilerplate code with first edit

---
+++ maxwell-exerercise-4.4.js
@@ -0,0 +1,44 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Assignment 4.4
+ Today's Date
+
+ -- ORIGINAL ARRAY --
+ Alabama
+ Nebraska
+ Iowa
+ California
+ Nevada
+
+ -- SORTED ARRAY --
+ Alabama
+ California
+ Iowa
+ Nebraska
+ Nevada
+
+ -- SELECTED VALUE --
+ Iowa
+
+ */
+
+// start program
+// create an array and assign it to the variable x
+var x = ["Alabama", "California", "Iowa", "Nebrasks", "Nevada"];
+
+// function
+function getValue(x, val) {
+
+}
+
+
+// Output
+
+
+
+
+// end program
