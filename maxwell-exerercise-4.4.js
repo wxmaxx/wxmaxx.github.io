40cfb7c39d9f7ad37ed2b68618d2021567e922d9
Leon <wxmaxx@gmail.com>
2018-06-13 05:26:33 -0500

    got the unsorted array to log

--- maxwell-exerercise-4.4.js
+++ maxwell-exerercise-4.4.js
@@ -28,15 +28,29 @@ var header = require('../header.js');
 
 // start program
 // create an array and assign it to the variable x
-var x = ["Alabama", "California", "Iowa", "Nebrasks", "Nevada"];
+var x = ["Alabama", "Nebraska", "Iowa", "California", "Nevada"];
 
-// function
+// functions
+// getValue function uses for loop to get array values the uses if statement to compare values
 function getValue(x, val) {
+ for (var k = 0; k < arr.length; k++) {
+   if (x[k] === val)
+     console.log(x[k]);
+ }
 
 }
 
 
 // Output
+// display the original unsorted array by looping through the array and logging the output
+console.log("--DISPLAYING ORIGINAL UNSORTED ARRAY--");
+for (var y = 0; y < x.length; y++)
+console.log(x[y]);
+
+// new line
+console.log("");
+
+//
 
 
 
