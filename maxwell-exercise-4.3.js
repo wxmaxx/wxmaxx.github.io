40fecff94198cc3f6443d542c44521009866fff1
Leon <wxmaxx@gmail.com>
2018-06-13 04:12:19 -0500

    Starter code

---
+++ maxwell-exercise-4.3.js
@@ -0,0 +1,41 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 4.3
+ Today's Date
+
+ -- DISPLAYING ARRAY ITEMS --
+ Car
+ Truck
+ Motorcycle
+ Airplane
+ Bus
+
+ -- SELECTED VALUE --
+ Motorcycle
+
+ -- SELECTED VALUE --
+ Bus
+
+ */
+
+// start program
+
+// create an array of vehicles and assign it to the variable vehicles
+var vehicles = ["Car", "Truck", "Motorcycle", "Airplane", "Bus"]; 
+
+// function
+function getValue(arr, val) {
+
+
+}
+
+
+// Output
+
+
+
+// end program
